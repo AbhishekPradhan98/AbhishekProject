a599de16dfcc59e687d3a8a088b87841926f5406
abhis <abhis@Abhishek_Gaur>
2022-11-23 13:28:28 +0530

    first

---
+++ src/Abhi/firstprog.java
@@ -0,0 +1,10 @@
+package Abhi;
+
+public class firstprog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("this is first program");
+	}
+
+}
