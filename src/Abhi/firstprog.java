fb18a3fab5869139fcc20a8e7e85c2e5afec09fd
abhis <abhis@Abhishek_Gaur>
2022-11-23 14:21:16 +0530

    modified

--- src/Abhi/firstprog.java
+++ src/Abhi/firstprog.java
@@ -5,6 +5,7 @@ public class firstprog {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("this is first program");
+         System.out.println("this is secon program");
 	}
 
 }
